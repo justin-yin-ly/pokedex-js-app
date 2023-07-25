59e0f5c2b78562ce1da8788fc7cd4d57950db912
jl2032 <55512607+jl2032@users.noreply.github.com>
2023-07-25 13:03:22 -0400

    Added a forloop that displays information from pokemonList. The forloop also contains a conditional to display bonus text depending on the height of the pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ let pokemonList = [
 
     {
         name: 'Charmander',
-        height: 0.6,
+        height: 1.6,
         types: ['fire']
     },
 
@@ -17,3 +17,14 @@ let pokemonList = [
         types: ['water']
     }
 ];
+
+for(let i = 0; i < 3; i++)
+{
+    let pokeInfo = pokemonList[i].name + '(height: ' + pokemonList[i].height + ')';
+    if(pokemonList[i].height > 1)
+    {
+        pokeInfo += ' - Wow, that\'s big!';
+    }
+    pokeInfo += '<br><br>'
+    document.write(pokeInfo);
+}
