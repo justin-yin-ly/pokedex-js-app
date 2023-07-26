024d6e6a66d9787bb0b43abba9b2f5bee59fd52d
jl2032 <55512607+jl2032@users.noreply.github.com>
2023-07-26 15:05:32 -0400

    Updated scripts.js according to Task 1.5
    Wrapped pokemonList in IIFE and set new variable pokemonRepository to the IIFE's return. Adjusted displayPokeArray call to reflect the  accessibility changes to pokemonList. Changed displayPokeArray to use a forEach loop instead of a for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,48 @@
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 0.7,
-        types: ['grass','poison']
-    },
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 0.7,
+            types: ['grass','poison']
+        },
 
-    {
-        name: 'Charmander',
-        height: 1.6,
-        types: ['fire']
-    },
+        {
+            name: 'Charmander',
+            height: 1.6,
+            types: ['fire']
+        },
 
-    {
-        name: 'Squirtle',
-        height: 0.5,
-        types: ['water']
+        {
+            name: 'Squirtle',
+            height: 0.5,
+            types: ['water']
+        }
+    ];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-];
+
+    function getAll()
+    {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
 
 
 function displayPokeArray (list)
 {
-    for(let i = 0; i < list.length; i++)
-    {
-        let pokeInfo = list[i].name + '(height: ' + list[i].height + ')';
-        if(list[i].height > 1)
-        {
-            pokeInfo += ' - Wow, that\'s big!';
-        }
-        pokeInfo += '<br><br>'
-        document.write(pokeInfo);
-    }
+    list.forEach(function(pokemon) {
+        document.write(
+            pokemon.name + ' (height: ' + pokemon.height + ')' + 
+            (pokemon.height > 1 ? ' - Wow, that\'s big!': '') + '<br><br>'
+            );
+    });
 }
 
-displayPokeArray(pokemonList);
+displayPokeArray(pokemonRepository.getAll());
