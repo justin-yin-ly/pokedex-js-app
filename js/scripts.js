cd5b79359812a7a62ba235829e603104952588cf
jl2032 <55512607+jl2032@users.noreply.github.com>
2023-07-31 17:00:30 -0400

    Updated index.html, styles.css, and scripts.js
    index.html: Added new div with id 'modal-container' to work with new modal functionality.
    
    styles.css: Added stylings to modal-container and any modal that gets created. Added media queries to make webpage more responsive; on smaller screens, the buttons should collapse into two columns and the modal should resize appropriately. Also made a minor change to some style rules after I realized I could use 'background' instead of 'background-color'
    
    scripts.js: Added new modal functionality; now, instead of simply printing to the console, the buttons display a modal that tells the user information about a particular pokemon.

--- js/scripts.js
+++ js/scripts.js
@@ -43,7 +43,7 @@ let pokemonRepository = (function () {
         button.classList.add('poke-button');
         button.addEventListener('click', function() {
             loadDetails(pokemon).then(function () {
-                console.log(pokemon);
+                showModal(pokemon.imageUrl, pokemon.name, 'Height: ' + pokemon.height);
             });
         });
         listItem.appendChild(button);
@@ -106,3 +106,52 @@ pokemonRepository.loadList().then(function() {
         pokemonRepository.addListItem(pokemon);
     });
 });
+
+function showModal(spriteUrl, title, text) {
+    let modalContainer = document.querySelector('#modal-container');
+  
+    modalContainer.innerText = '';
+    
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+  
+    let spriteElement = document.createElement('img');
+    spriteElement.src = spriteUrl;
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+  
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+  
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal)
+
+    modal.appendChild(spriteElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modal.appendChild(closeButtonElement);
+    modalContainer.appendChild(modal);
+  
+    modalContainer.classList.add('is-visible');
+    modalContainer.addEventListener('click', (e) => {
+      let target = e.target;
+      if (target === modalContainer) {
+        hideModal();
+      }
+    });
+  }
+  
+  function hideModal() {
+    let modalContainer = document.querySelector('#modal-container');
+    modalContainer.classList.remove('is-visible');
+  }
+  
+  window.addEventListener('keydown', (e) => {
+    let modalContainer = document.querySelector('#modal-container');
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  })
